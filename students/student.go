d97e7272c0a8d7e6c0a6b2988e0e31ce00ee05e0
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-14 20:15:02 +0100

    Student Struct and Constants Added

--- students/student.go
+++ students/student.go
@@ -1,1 +1,17 @@
 package students
+
+type Student struct {
+	ID 					int
+	FirstName, LastName string
+	age 				int
+	class				int
+}
+
+const (
+	JSS = iota
+	JSS1 = iota
+	JSS3
+	SSS1
+	SSS2
+	SSS3
+)
