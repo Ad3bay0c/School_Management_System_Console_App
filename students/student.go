dbd60b285da9725e13bf52cb20aa45251ff1149a
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-14 23:58:38 +0100

    Student Struct Function Modified

--- students/student.go
+++ students/student.go
@@ -3,15 +3,6 @@ package students
 type Student struct {
 	ID 					int
 	FirstName, LastName string
-	age 				int
-	class				int
+	Age 				int
+	Class				int
 }
-
-const (
-	JSS = iota
-	JSS1 = iota
-	JSS3
-	SSS1
-	SSS2
-	SSS3
-)
