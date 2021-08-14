d99bdf7b60921fa2970c5f9a359cd0c6871ff9cd
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-14 22:40:01 +0100

    List of Students Added

--- school/students.go
+++ school/students.go
@@ -1,1 +1,27 @@
 package school
+
+import "github.com/Ad3bay0c/SchoolManagement/students"
+
+var Students = map[int]students.Student{
+	1: {
+		ID: 1,
+		FirstName: "Clin",
+		LastName:  "Ade",
+		Age:       15,
+		Class:     SSS1,
+	},
+	2: {
+		ID:        2,
+		FirstName: "Clint",
+		LastName:  "Ade",
+		Age:       15,
+		Class:     SSS2,
+	},
+	3: {
+		ID:        3,
+		FirstName: "Clinto",
+		LastName:  "Ade",
+		Age:       15,
+		Class:     SSS3,
+	},
+}
