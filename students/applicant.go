c9f2718d02205f35f3848379160743c60f37b7bd
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-15 22:03:51 +0100

    CheckStatus Method Added to Applicant

--- students/applicant.go
+++ students/applicant.go
@@ -1,7 +1,15 @@
 package students
 
+import "github.com/Ad3bay0c/SchoolManagement/school"
+
 type Applicant struct {
 	ID 					int
 	FirstName, LastName string
 	Age 				int
+}
+
+func(applicant Applicant) CheckStatus() bool {
+	_, err := school.Students[applicant.ID]
+
+	return err
 }
