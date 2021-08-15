64e189d9255a0f00b0a6384e1149e3547bfead2b
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-15 20:58:14 +0100

    GradeStudent Method for Teacher

--- staff/teacher.go
+++ staff/teacher.go
@@ -1,1 +1,17 @@
 package staff
+
+import "github.com/Ad3bay0c/SchoolManagement/school"
+
+type Teacher struct {
+	ID int
+	FirstName, LastName string
+	Role                int
+}
+
+func (teacher Teacher) GradeStudent(student *school.Student) {
+	student.Grade = map[string]string{
+		"English" : "A",
+		"Mathematics" : "B",
+		"Chemistry" : "A1",
+	}
+}
