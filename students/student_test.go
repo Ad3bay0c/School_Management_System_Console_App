92935b68ece941ef2a4e7c646a4c6842cf115483
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-15 21:51:11 +0100

    Check Grade test file  Modified

--- students/student_test.go
+++ students/student_test.go
@@ -32,3 +32,32 @@ func TestStudent_TakeCourse(t *testing.T) {
 		})
 	}
 }
+
+func TestStudent_CheckGrade(t *testing.T) {
+	tables := []struct{
+		input 	Student
+		output 	int
+		word   	string
+	}{
+		{
+			input: Students[1],
+			output: 0,
+			word: "Check Grade 1",
+		},
+		{
+			input: Students[2],
+			output: 0,
+			word: "Check Grade 2",
+		},
+	}
+
+	for _, table := range tables {
+		t.Run(table.word, func(t *testing.T) {
+			result := table.input.CheckGrade()
+
+			if len(result) != table.output {
+				t.Errorf("Expected %v; Got %v", table.output, len(result))
+			}
+		})
+	}
+}
