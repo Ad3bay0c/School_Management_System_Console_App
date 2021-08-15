a6af2881b5cc5de454207b09a6b0f39ea8b781a0
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-15 22:14:35 +0100

    TestApplicant_CheckStatus Added

--- students/applicant_test.go
+++ students/applicant_test.go
@@ -1,1 +1,37 @@
 package students
+
+import (
+	"testing"
+)
+
+func TestApplicant_CheckStatus(t *testing.T) {
+	applicants := []struct{
+		input		Applicant
+		output		bool
+		word 		string
+	}{
+		{
+			input: Applicant{
+				ID: 2,
+			},
+			output: true,
+			word: "Check Status 1",
+		},
+		{
+			input: Applicant{
+				ID: 20,
+			},
+			output: false,
+			word: "Check Status 1",
+		},
+	}
+
+	for _, applicant := range applicants {
+		t.Run(applicant.word, func(t *testing.T) {
+			result := applicant.input.CheckStatus()
+			if result != applicant.output {
+				t.Errorf("Expected %v; Got %v", applicant.output, result)
+			}
+		})
+	}
+}
