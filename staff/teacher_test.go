259e0916b5331681114a766b4309c9636ef25ebf
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-15 21:10:30 +0100

    TestTeacher_GradeStudent Added

---
+++ staff/teacher_test.go
@@ -0,0 +1,34 @@
+package staff
+
+import (
+	"github.com/Ad3bay0c/SchoolManagement/school"
+	"testing"
+)
+
+func TestTeacher_GradeStudent(t *testing.T) {
+	students := []struct{
+		input		school.Student
+		output		int
+		word 		string
+	} {
+		{
+			input: school.Students[1],
+			output: 3,
+			word: "Grade Student 1",
+		},
+	}
+	teacher := Teacher{
+		ID:        1,
+		FirstName: "Mr Ojo",
+		LastName:  "L",
+		Role:      school.TEACHER,
+	}
+	for _, student := range students {
+		t.Run(student.word, func(t *testing.T) {
+			teacher.GradeStudent(&student.input)
+			if len(student.input.Grade) != student.output {
+				t.Errorf("Expected %v; Got %v", student.output, len(student.input.Grade))
+			}
+		})
+	}
+}
