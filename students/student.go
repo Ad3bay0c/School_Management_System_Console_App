5ff7d478dfcfedf086946e343b3028a83f4abc74
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-15 19:52:17 +0100

    Method Take Course Added for Student

--- students/student.go
+++ students/student.go
@@ -1,8 +1,20 @@
 package students
 
+import "github.com/Ad3bay0c/SchoolManagement/school"
+
 type Student struct {
 	ID 					int
 	FirstName, LastName string
 	Age 				int
 	Class				int
+	Subject				[]string
 }
+
+func (s Student) TakeCourse() bool {
+	s.Subject = school.Subject[s.Class]
+	if s.Subject[1] != "" {
+		return true
+	}
+	return false
+}
+
