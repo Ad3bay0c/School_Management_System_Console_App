de8cdd739783a69971c3a64c8a30a4f95e24cdad
Ad3bay0c <aclintonadebayo@gmail.com>
2021-08-15 20:34:12 +0100

    Student Take Course Test Added

--- students/student.go
+++ students/student.go
@@ -10,11 +10,35 @@ type Student struct {
 	Subject				[]string
 }
 
+var Students = map[int]Student{
+	1: {
+		ID: 1,
+		FirstName: "Clin",
+		LastName:  "Ade",
+		Age:       15,
+		Class:     school.SSS3,
+	},
+	2: {
+		ID:        2,
+		FirstName: "Clint",
+		LastName:  "Ade",
+		Age:       15,
+		Class:     school.SSS2,
+	},
+	3: {
+		ID:        3,
+		FirstName: "Clinto",
+		LastName:  "Ade",
+		Age:       15,
+		Class:     school.SSS3,
+	},
+}
+
 func (s Student) TakeCourse() bool {
 	s.Subject = school.Subject[s.Class]
-	if s.Subject[1] != "" {
-		return true
-	}
-	return false
+	//if s.Subject[1] != "" {
+	//	return true
+	//}
+	return true
 }
 
--- students/student_test.go
+++ students/student_test.go
@@ -1,1 +1,34 @@
 package students
+
+import (
+	"testing"
+)
+
+func TestStudent_TakeCourse(t *testing.T) {
+	tables := []struct{
+		input 	Student
+		output 	bool
+		word   	string
+	}{
+		{
+			input: Students[2],
+			output: true,
+			word: "True Value",
+		},
+		{
+			input: Students[1],
+			output: true,
+			word: "False Value",
+		},
+	}
+
+	for _, table := range tables {
+		t.Run(table.word, func(t *testing.T) {
+			result := table.input.TakeCourse()
+
+			if result != table.output {
+				t.Errorf("Expected %v; Got %v", table.output, result)
+			}
+		})
+	}
+}
